7aabd539a82b1ac95551065237ffb5e628c6a2bb
Souptikbej <bejsouptik@gmail.com>
2025-10-15 19:11:58 +0530

    day5

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,66 @@
-import React, { useState } from "react";
+import React from "react";
 
 const App = () => {
-  const [unam, setA] = useState("Souptik Bej");
-  const [num , setB] = useState(0);
-  const change = () => {
-    setA("Sutapa Bej");
-  };
   return (
     <div>
-      <h1>User Name :{unam}</h1>
-      <button onClick={change}>Change</button>
-      <h1>Add to cart : {num}</h1>
-      <button onClick={()=>{setB(num+1)}}>+</button>
-      <button onClick={()=>{setB(num-1)}}>-</button>
+      <table>
+        <thead>
+          <tr>
+            <th>Chief Patrons:</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>
+              Padma Vibhushan Prof. M. M. Sharma, Emeritus Professor of
+              Eminence, ICT Mumbai.
+            </td>
+          </tr>
+          <tr>
+            <td>
+              Padma Shri Prof. G. D. Yadav, Bhatnagar Fellow and National
+              Science Chair and Emeritus Professor of Eminence, Former VC, ICT
+              Mumbai.
+            </td>
+          </tr>
+          <tr>
+            <td>
+              Padma Shri Prof. Ashutosh Sharma, President, INSA, New Delhi,
+              Institute Chair Professor, IIT Kanpur.
+            </td>
+          </tr>
+        </tbody>
+        <thead>
+          <tr>
+            <th>Members:</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>
+              Prof. Ajay Bansal, President, IIChE & Registrar, NIT, Jalandhar.
+            </td>
+          </tr>
+          <tr>
+            <td>Prof. AB Pandit, Vice Chancellor, ICT, Mumbai.</td>
+          </tr>
+          <tr>
+            <td>
+              Prof. M.K. Jha, NIT, Jalandhar & Director General, IIChE Training
+              Institute, IIChE.
+            </td>
+          </tr>
+          <tr>
+            <td>Shri Praveen Saxena, Bhumistha Graphite Power, Mumbai.</td>
+          </tr>
+          <tr>
+            <td>
+              Prof. S.V. Satyanarayana, Director, Academic & Planning, JNTUA,
+              Ananthapuramu.
+            </td>
+          </tr>
+        </tbody>
+      </table>
     </div>
   );
 };
