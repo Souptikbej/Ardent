2320800df4cb838d00bbdcef704477a92e22ba77
Souptikbej <bejsouptik@gmail.com>
2025-10-23 19:49:51 +0300

    day7

--- src/examples/Reschessboard.jsx
+++ src/examples/Reschessboard.jsx
@@ -1,23 +1,43 @@
 import React from "react";
-
+// import Tree from '../assets/tree.jpg';
+import Manone from "../assets/Manone.png";
+import Mantwo from "../assets/Mantwo.png";
+import Manthree from "../assets/Manthree.png";
+import Manfour from "../assets/Manfour.png";
 const Reschessboard = () => {
   return (
-    <div class="container">
+    <div class="container my-5">
       <h3 class="text-center mb-4 section-title">
         💠 Board of Governance Members
       </h3>
       <div class="row g-4">
         <div class="col-lg-3 col-md-6 col-12">
-          <div class="card"></div>
+          <div class="card">
+            <img src={Manone} class="card-img-top" alt="Profile" />
+            <p class="h6 card-title mb-1">Dr. Avijit Ghosh</p>
+            <span class="designation">Secretary, CEIE</span>
+          </div>
         </div>
         <div class="col-lg-3 col-md-6 col-12">
-          <div class="card"></div>
+          <div class="card">
+            <img src={Mantwo} class="card-img-top" alt="Profile" />
+            <p class="h6 card-title mb-1">Prof.(Dr.) Subrata Mondal</p>
+            <span class="designation">BOG Member, CEIE</span>
+          </div>
         </div>
         <div class="col-lg-3 col-md-6 col-12">
-          <div class="card"></div>
+          <div class="card">
+            <img src={Manthree} class="card-img-top" alt="Profile" />
+            <p class="h6 card-title mb-1">Mr. Amal Kumar Dutta</p>
+            <span class="designation">BOG Member, CEIE</span>
+          </div>
         </div>
         <div class="col-lg-3 col-md-6 col-12">
-          <div class="card"></div>
+          <div class="card">
+            <img src={Manfour} class="card-img-top" alt="Profile" />
+            <p class="h6 card-title mb-1">Prof.(Dr.) Biswajit Mandal</p>
+            <span class="designation">BOG Member, CEIE</span>
+          </div>
         </div>
       </div>
     </div>
